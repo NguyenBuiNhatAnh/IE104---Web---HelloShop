ec60dc8c953bdbe84bf0aa02fdfd96096edd2b70
PRAY\Admin <23521157@gm.uit.edu.vn>
2025-11-28 19:34:03 +0700

    them trang register, chinh sua trang login

--- javascript/login.js
+++ javascript/login.js
@@ -3,33 +3,47 @@ import { token } from "../sharedata/sharedata.js";
 
 // Hàm login
 export function login(event) {
-    let logined = false;
-    event.preventDefault();
-    const email = document.getElementById('email').value;
-    const password = document.getElementById('password').value;
+  let logined = false;
+  event.preventDefault();
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
 
-    if(email === "hello@gmail.com" & password === "helloshop") {
-        token.value = "user";
-        localStorage.setItem("token", token.value)
-        window.location.href = window.location.origin + "/#/";
-        showPopup("Loggin successfully!", "Đăng nhập thành công!")
-        return;
-    }
-    else {
-        logined = true;
-    }
-    if(email === "admin@gmail.com" & password === "admin") {
-        token.value = "admin";
-        localStorage.setItem("token", token.value)
-        window.location.href = window.location.origin + "/#/admin";
-        showPopup("Loggin successfully!", "Đăng nhập thành công!")
-        return;
-    }
-    else {
-        logined = true;
-    }
-    if(logined) {
-        showPopup("The username or password is incorrect!","Sai tài khoản hoặc mật khẩu!",true)
-    }
+  if ((email === "hello@gmail.com") & (password === "helloshop")) {
+    token.value = "user";
+    localStorage.setItem("token", token.value);
+    window.location.href = window.location.origin + "/#/";
+    showPopup("Loggin successfully!", "Đăng nhập thành công!");
+    return;
+  } else {
+    logined = true;
+  }
+  if ((email === "admin@gmail.com") & (password === "admin")) {
+    token.value = "admin";
+    localStorage.setItem("token", token.value);
+    window.location.href = window.location.origin + "/#/admin";
+    showPopup("Loggin successfully!", "Đăng nhập thành công!");
+    return;
+  } else {
+    logined = true;
+  }
+  const users = JSON.parse(localStorage.getItem("registeredUsers")) || [];
+  const foundUser = users.find(
+    (u) => u.email === email && u.password === password
+  );
+
+  if (foundUser) {
+    token.value = "user";
+    localStorage.setItem("token", token.value);
+    window.location.href = window.location.origin + "/#/";
+    showPopup("Loggin successfully!", "Đăng nhập thành công!");
+    return;
+  }
+  if (logined) {
+    showPopup(
+      "The username or password is incorrect!",
+      "Sai tài khoản hoặc mật khẩu!",
+      true
+    );
+  }
 }
 window.login = login;
